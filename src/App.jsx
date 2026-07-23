f628565c3d73d311c89a4173c35095435fb93e6f
Soliceee <soliceee@users.noreply.github.com>
2026-07-23 18:40:32 +0800

    Fix: wrap ParticlesProvider around StarBackground

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { ParticlesProvider } from '@tsparticles/react'
+import { loadAll } from '@tsparticles/all'
 import StarBackground from './StarBackground'
 
 function App() {
-  return <StarBackground />
+  return (
+    <ParticlesProvider init={async (engine) => { await loadAll(engine) }}>
+      <StarBackground />
+    </ParticlesProvider>
+  )
 }
 
 export default App
